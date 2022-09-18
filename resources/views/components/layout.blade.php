a4798d45fe085c1f5ce8b4f1ee4cec97240e5c88
Alvis Balodis <53913245+AlvisBalodis@users.noreply.github.com>
2022-09-18 13:02:18 +0300

    Update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link rel="icon" href="images/favicon.ico"/>
-    {{--    <link--}}
-    {{--        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"--}}
-    {{--        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="--}}
-    {{--        crossorigin="anonymous"--}}
-    {{--        referrerpolicy="no-referrer"/>--}}
 
     <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
@@ -106,6 +101,7 @@
        class="drop-shadow-md absolute top-1/5 right-10 bg-black text-white py-3 px-5 rounded-3xl hover:opacity-80">Add
         Asset</a>
 </footer>
+
 <x-flash-message/>
 
 </body>
